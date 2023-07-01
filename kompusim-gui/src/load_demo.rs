071ba6e770b5b248bfdd4b6d3853aebf5d456a98
Dmitry Voytik <voytikd@gmail.com>
2023-07-01 23:33:27 +0200

    ui: load_demo: add struct Demo
    Signed-off-by: Dmitry Voytik <voytikd@gmail.com>

--- kompusim-gui/src/load_demo.rs
+++ kompusim-gui/src/load_demo.rs
@@ -1,18 +1,42 @@
 pub struct LoadDemo {
     /// Is window open or not
     window_open: bool,
-    test_bin1: &'static [u8],
+    demos: Vec<Demo>,
+    selected_demo: usize,
 }
 
 impl Default for LoadDemo {
     fn default() -> LoadDemo {
-        let bare_metal_uart_hello_world =
-            include_bytes!("../assets/test_bins/uart_hello_world.bin");
+        let mut demos = Vec::new();
+        demos.push(Demo {
+            name: "Hello world",
+            descriptoin: "Bare metall program printing Hello World to UART",
+            load_address: 0x0000000080000000,
+            breakpoint: 0x0000000080000014,
+            image: include_bytes!("../assets/test_bins/uart_hello_world.bin"),
+        });
+        // TODO: changes this:
+        demos.push(Demo {
+            name: "Hello world",
+            descriptoin: "Bare metall program printing Hello World to UART",
+            load_address: 0x0000000080000000,
+            breakpoint: 0x0000000080000014,
+            image: demos[0].image,
+        });
         LoadDemo {
             window_open: true,
-            test_bin1: bare_metal_uart_hello_world,
+            demos,
+            selected_demo: 0,
         }
     }
+}
+
+struct Demo {
+    name: &'static str,
+    descriptoin: &'static str,
+    load_address: u64,
+    breakpoint: u64,
+    image: &'static [u8],
 }
 
 impl LoadDemo {
@@ -21,28 +45,41 @@ impl LoadDemo {
     }
 
     pub fn show(&mut self, ctx: &egui::Context) -> Option<&'static [u8]> {
-        let mut chose_demo1 = false;
+        let mut loaded_demo = false;
         if self.window_open {
             let mut window_opened = self.window_open;
             egui::Window::new("Load demo")
                 .open(&mut window_opened)
                 .resizable(true)
-                .default_width(400.0)
+                .default_width(500.0)
                 .show(ctx, |ui| {
                     egui::Grid::new("load_demo_grid")
-                        .num_columns(2)
-                        .spacing([40.0, 4.0])
+                        .num_columns(1)
+                        .min_col_width(600.0)
                         .striped(true)
                         .show(ui, |ui| {
-                            ui.label("Bare metal hello world with UART");
-                            if ui.button("Load").clicked() {
-                                self.window_open = false;
-                                chose_demo1 = true;
-                            }
+                            egui::ComboBox::from_label("").width(400.0).show_index(
+                                ui,
+                                &mut self.selected_demo,
+                                self.demos.len(),
+                                |i| self.demos[i].name,
+                            );
                             ui.end_row();
-                            ui.label("Linux kernel (unimplemented)");
+                            ui.label(self.demos[self.selected_demo].descriptoin);
+                            ui.end_row();
+                            ui.label(format!(
+                                "Load address: 0x{:x}",
+                                self.demos[self.selected_demo].load_address
+                            ));
+                            ui.end_row();
+                            ui.label(format!(
+                                "Breakpoint: 0x{:x}",
+                                self.demos[self.selected_demo].breakpoint
+                            ));
+                            ui.end_row();
                             if ui.button("Load").clicked() {
                                 self.window_open = false;
+                                loaded_demo = true;
                             }
                             ui.end_row();
                         });
@@ -51,8 +88,8 @@ impl LoadDemo {
                 self.window_open = window_opened;
             }
         }
-        if chose_demo1 {
-            Some(self.test_bin1)
+        if loaded_demo {
+            Some(self.demos[self.selected_demo].image)
         } else {
             None
         }
