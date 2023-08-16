cf9397b23495196e131e411922400201d04ecd37
Dmitry Voytik <voytikd@gmail.com>
2023-08-16 23:56:57 +0200

    ui: BaseURegs: implement highlight of registers with grid_row_reg()
    Signed-off-by: Dmitry Voytik <voytikd@gmail.com>

--- kompusim-gui/src/base_uregs.rs
+++ kompusim-gui/src/base_uregs.rs
@@ -38,7 +38,7 @@ impl BaseURegs {
                         .striped(true)
                         .show(ui, |ui| {
                             for i in 0..=15 {
-                                grid_row_reg(ui, regs, i as u8);
+                                grid_row_reg(ui, regs, i as u8, reg_hi_color());
                             }
                         });
                     egui::Grid::new("base_regs_grid1")
@@ -47,7 +47,7 @@ impl BaseURegs {
                         .striped(true)
                         .show(ui, |ui| {
                             for i in 16..=31 {
-                                grid_row_reg(ui, regs, i as u8);
+                                grid_row_reg(ui, regs, i as u8, reg_hi_color());
                             }
                             ui.label(format!("pc"));
                             ui.label("");
@@ -60,10 +60,25 @@ impl BaseURegs {
     }
 }
 
+fn reg_hi_color() -> Option<egui::Color32> {
+    // TODO: input (Option<u8>, Option<u8>, Option<u8>) - (read_reg, read_reg, write_reg)
+    // egui::Color32::YELLOW;
+    None
+}
+
 /// show a register in the grid
-fn grid_row_reg(ui: &mut egui::Ui, regs: &RV64IURegs, ri: u8) {
-    ui.label(format!("x{ri}"));
-    ui.label(reg_idx2abi(ri));
-    ui.label(reg_hex(regs.x[ri as usize]));
+fn grid_row_reg(ui: &mut egui::Ui, regs: &RV64IURegs, ri: u8, hi_color: Option<egui::Color32>) {
+    let reg_name = format!("x{ri}");
+    let reg_abi_name = reg_idx2abi(ri);
+    let reg_hex_val = reg_hex(regs.x[ri as usize]);
+    if let Some(color) = hi_color {
+        ui.colored_label(color, reg_name);
+        ui.colored_label(color, reg_abi_name);
+        ui.colored_label(color, reg_hex_val);
+    } else {
+        ui.label(reg_name);
+        ui.label(reg_abi_name);
+        ui.label(reg_hex_val);
+    }
     ui.end_row();
 }
